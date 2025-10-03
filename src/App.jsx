f7494bdfcfed98b8b1efabcbfb726974a1e91928
Vitor Hugo <ovitorhugoalvesmachado@gmail.com>
2025-10-03 18:45:51 -0300

    Atualizar o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -182,7 +182,7 @@ function App() {
   href="https://docs.google.com/document/d/1VTjPyx07QO_1-9lqp1qiAODz-dfFM3oE/edit?usp=sharing&ouid=102900673067841125183&rtpof=true&sd=true"
   target="_blank"
   rel="noopener noreferrer"
-  class="inline-flex items-center gap-2 px-5 py-2.5 rounded-lg font-medium text-sm
+  class="inline-flex items-center gap-2 mt-4 text-white px-5 py-2.5 rounded-lg font-medium text-sm
          bg-white/20 backdrop-blur-md border border-white/30
          hover:bg-white/30"
 >
